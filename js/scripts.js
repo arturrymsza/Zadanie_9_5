1ce382dd339e7ad03a6368fe3f82bdd7bf3ce6fd
Artur Rymsza <artur.rymsza@gmail.com>
2017-12-04 17:48:47 +0100

    Add changes to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var buttonClass = document.getElementsByClassName('button');
-for (i = 0; i < buttonClass.length; i++) {
+var buttonLength = buttonClass.length;
+for (var i = 0; i < buttonLength; i++) {
 	alert(buttonClass[i].innerText);
 }
